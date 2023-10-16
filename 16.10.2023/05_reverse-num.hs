069598d81a9eee326d12aea5a6cab166c905ec6f
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-10-16 11:15:04 +0300

    solved problem

---
+++ 16.10.2023/05_reverse-num.hs
@@ -0,0 +1,13 @@
+reverseNum :: Int -> Int
+reverseNum n =
+            if ( n < 10 )
+                then n
+            else
+                read ( reverse (show n) )
+
+main = do
+    input <- getLine
+    let num = read input :: Int
+    let result = reverseNum num
+    print result
+
