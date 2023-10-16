40f65af5932453f4d1772cf832e7b92c0712fc4a
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2023-10-16 08:26:02 +0300

    bug fix

--- Recoursion/sumOf1toN.hs
+++ Recoursion/sumOf1toN.hs
@@ -1,10 +1,13 @@
-main = do  
+main :: IO ()
+main = do
     line <- getLine
     let numbers = read line :: Int
-    sumaNumber = sumaLoop 0 1 numbers
-    sumaLoop sum index numbers =
-                                    if (index > numbers)
-                                        then sum
-                                    else
-                                        sumaLoop (sum - index) (index + 1) numbers
+        sumaNumber = sumaLoop 0 1 numbers
     print sumaNumber
+
+sumaLoop :: Int -> Int -> Int -> Int
+sumaLoop sum index numbers =
+    if index > numbers
+        then sum
+    else
+        sumaLoop (sum + index) (index + 1) numbers
