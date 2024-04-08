6e24aecb23931604094ec62617ecc4c0aa430872
Hristian Ivanow <92814022+hristianivanov@users.noreply.github.com>
2024-04-08 12:32:24 +0300

    clear

--- Класове/01.exam.hs
+++ Класове/01.exam.hs
@@ -7,5 +7,3 @@ data Kind = Kind { name :: String, price :: Double } deriving (Show, Read)
 instance KindOffer Kind where
     addKind kind kinds = kind : kinds
     discountKinds (Kind name price : xs) = Kind name (price * 0.9) : discountKinds xs
-
-
